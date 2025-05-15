6512c043a95408041638fa8ce95e2b8f135319d4
Tatty <pereyratatty@gmail.com>
2025-05-15 16:26:08 -0300

    Tres primerps mensajes

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-const saludo = "Hola, empezamos con el primer proyecto
-"
+const saludo = "Hola, empezamos con el primer proyecto"
+const saludo = "Vamos a ver si entendemos un poco"
+const saludo = "Ojala pronto agarremos cancha"
